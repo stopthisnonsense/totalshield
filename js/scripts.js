338d5735e53a4eb5dd6f56dd22c6daef20148ec9
StopNonsense <anthonyjmarvin@gmail.com>
2022-02-10 10:08:00 -0500

    transpile to es5

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
-//Place your JavaScript of jQuery snippets here. Remember to remove the <script> tags
+'use strict';
+
 jQuery(document).ready(function ($) {
 	function gallerySet(item) {
 		var gallery = item;
-		var selector = `${item} .et_pb_image_wrap`;
+		var selector = item + ' .et_pb_image_wrap';
 		$(selector).each(function (index) {
 			itemIndex = index;
 			$(this).data('slider', index);
 			$(this).on('click', function () {
 				$(selector).removeClass('active');
 				$(this).addClass('active');
-				itemIndex = ` ${gallery} .et-pb-controllers a:nth-child(${index + 1})`;
+				itemIndex =
+					' ' +
+					gallery +
+					' .et-pb-controllers a:nth-child(' +
+					(index + 1) +
+					')';
 				$(itemIndex).trigger('click');
 			});
 		});
 	}
-	gallerySet(`.js--gallery-1`);
-	gallerySet(`.js--gallery-2`);
-	gallerySet(`.js--gallery-3`);
-	gallerySet(`.js--gallery-4`);
+	gallerySet('.js--gallery-1');
+	gallerySet('.js--gallery-2');
+	gallerySet('.js--gallery-3');
+	gallerySet('.js--gallery-4');
 });
